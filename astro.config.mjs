bb0aaeb4461fa49d2eff46efb1a8b75d6bafa449
Jérémy FRERE <frere.jeremy@gmail.com>
2025-09-30 20:14:26 +0200

    Updates URL with the real domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,28 @@ import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from "./src/i18n/locales";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-cms-dpv.pages.dev", // Set your site's URL
-	i18n: {
-		defaultLocale: DEFAULT_LOCALE_SETTING,
-		locales: Object.keys(LOCALES_SETTING),
-		routing: {
-			prefixDefaultLocale: true,
-			redirectToDefaultLocale: false,
-		},
-	},
-	integrations: [
-		mdx(),
-		sitemap({
-			filter: (page) => page !== `${BASE_URL}/admin/`,
-			i18n: {
-				defaultLocale: DEFAULT_LOCALE_SETTING,
-				locales: Object.fromEntries(
-					Object.entries(LOCALES_SETTING).map(([key, value]) => [
-						key,
-						value.lang ?? key,
-					]),
-				),
-			},
-		}),
-	],
+  site: "https://ensoleillade.info", // Set your site's URL
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => page !== `${BASE_URL}/admin/`,
+      i18n: {
+        defaultLocale: DEFAULT_LOCALE_SETTING,
+        locales: Object.fromEntries(
+          Object.entries(LOCALES_SETTING).map(([key, value]) => [
+            key,
+            value.lang ?? key,
+          ])
+        ),
+      },
+    }),
+  ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -24,5 +24,5 @@ export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
 
 // Base URLs
 const BASE_URL_LOCAL = "http://localhost:4321";
-const BASE_URL_PROD = "https://astro-cms-dpv.pages.dev";
+const BASE_URL_PROD = "https://ensoleillade.info";
 export const BASE_URL = import.meta.env.DEV ? BASE_URL_LOCAL : BASE_URL_PROD;
